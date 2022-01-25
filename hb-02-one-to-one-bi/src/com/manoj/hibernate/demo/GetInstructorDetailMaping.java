bc7d69cedf5cc41bb0da244e0c1c3616514723b1
U M MANOJ KUMAR <51526294+manojkumar9133@users.noreply.github.com>
2022-01-25 13:18:20 +0530

    Add files via upload

---
+++ hb-02-one-to-one-bi/src/com/manoj/hibernate/demo/GetInstructorDetailMaping.java
@@ -0,0 +1,53 @@
+package com.manoj.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.manoj.hibernate.demo.entity.Instructor;
+import com.manoj.hibernate.demo.entity.InstructorDetails;
+
+public class GetInstructorDetailMaping {
+
+	public static void main(String[] args) 
+	{
+		//create session factory
+		SessionFactory factory=new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetails.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session=factory.getCurrentSession();
+		
+		try
+		{
+			int theId=99;
+			
+			//start a trasaction
+			session.beginTransaction();
+			
+			InstructorDetails tempInstructorDetails=session.get(InstructorDetails.class, theId);
+			
+			System.out.println("temmpInstructorDetails : "+tempInstructorDetails);
+			
+			System.out.println("calling Instructor : "+tempInstructorDetails.getInstructor());
+
+			
+			//commit transcation
+			session.getTransaction().commit();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+			factory.close();
+		}
+		
+	}
+
+}
